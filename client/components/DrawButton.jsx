6d7e78f646ade9f8d9676ac61c058b48cc69041d
Carel Jan Starreveld <carelstarreveld@hotmail.com>
2018-11-09 12:25:24 +1300

    added card/drawbutton components

---
+++ client/components/DrawButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../server/public/flashCard.css'
+
+export default class DrawButton extends React.Component {
+  constructor (props) {
+    super(props)
+    this.drawCard = this.drawCard.bind(this)
+  }
+  drawCard () {
+    this.props.drawCard()
+  }
+  render (props) {
+    return (
+      <div className='buttonContainer'>
+        <button className='btn' onClick={this.drawCard}>Draw Card</button>
+      </div>
+    )
+  }
+}
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import cards from '../../server/public/cards.json'
+import DrawButton from './DrawButton'
+import Card from './Card'
 
 export default class Home extends React.Component {
-  
+  Constructor (props) {
+    Super(props)
+
+    this.updateCard = this.updateCard.bind(this)
+
+    this.state = {
+      cards: cards.Cards[0],
+      currentCard: {}
+    }
+  }
+  componentDidMount () {
+    const currentCard = this.state.cards
+    this.setState({
+
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard (currentCards) {
+    let card = currentCards[Math.floor(Math.random() * currentCards.length)]
+    return card
+  }
   render () {
     return (
       <div>
-        Hello
+        <div className='ccontainer'>
+          <div className='cardRow'>
+            <Card questions={this.state.currentCard.questions} />
+            <Card answers={this.state.currentCard.answers} />
+          This is a card
+          </div>
+        </div>
+        <div className='buttonRow'>
+          <DrawButton drawCard={this.updateCard} />
+        </div>
       </div>
+
     )
   }
 }
