ba656ce2202e22b5ca5f3d99f54b6a876bdd6bbf
Kiri Tavaga <kiri.tavaga@icloud.com>
2018-11-09 15:36:10 +1300

    tried writing to json file then localDB attempt

--- client/components/CreateCard.jsx
+++ client/components/CreateCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import cards from '../../server/public/cards.json'
+// import fs from 'fs'
+import * as localDb from '../localDb'
 
 export default class CreateCard extends React.Component {
   state = {
@@ -7,26 +10,38 @@ export default class CreateCard extends React.Component {
     answer:''
   }
 
+  componentDidMount () {
+    this.setState({
+      items: localDB.getItems()
+    })
+  }
   changeHandler = e => {
     this.setState({
       [e.target.name]: e.target.value
     })
   }
 
-  saveDataHandler = e => {
-    //we save data here
-  }
+  // saveDataHandler = e => {
+  //   let newCard = {questions:this.state.question, 
+  //     answers:this.state.answer}
+  //   let oldCards = JSON.parse(cards)
+  //   let newCards = oldCards.push(newCard)
+  //   // fs.writeFile('../../server/public/cards.json', JSON.stringify(newCards), function(err) {
+  //   //   if (err) {
+  //   //     return console.log('an error occured')
+  //   //   }
+  //   //   console.log("saved")
+  //   //   })
+  //   // }  
 
   render () {
-
-
     return (
       <div className='createCard'>
         <h2>Create Card</h2>
         <form>
           <input type='text' value={this.state.question} name='question' onChange={this.changeHandler} placeholder='Type your question here!' />
           <br /><br />
-          <input type='text' value={this.state.answer} name='question' onChange={this.changeHandler} placeholder='Type your answer!' />
+          <input type='text' value={this.state.answer} name='answer' onChange={this.changeHandler} placeholder='Type your answer!' />
           <br /><br /><br /><br />
           <button onClick={this.saveDataHandler}>
            Save and add new card
